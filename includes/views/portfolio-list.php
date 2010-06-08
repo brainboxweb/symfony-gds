44aba53baf412cddd5d71c35c25935e6707d861a
Gary Straughan <gary@brainboxweb.co.uk>
2010-06-08 12:00:47 +0000

    Adding skills

--- includes/views/portfolio-list.php
+++ includes/views/portfolio-list.php
@@ -1,13 +1,18 @@
 
-
-    
-    
+ 
+   
 <?php foreach($this->portfolioArray as $portfolioItem ): ?>
 
-    <h2><a href="/portfolio/<?php echo $portfolioItem->id ?>"><?php echo $portfolioItem->title ?></a></h2>
-    
-    <?php echo $portfolioItem->body ?>
-    
+     <div class="portfolio-item">
+     
+          <h2><a href="/portfolio/<?php echo $portfolioItem->id ?>"><?php echo $portfolioItem->title ?></a></h2>
+          
+          <?php echo $portfolioItem->metaDescription ?>
+          
+          <p><a href="/portfolio/<?php echo $portfolioItem->id ?>" rel="nofollow" ">Full details...</a></p>
+     
+     </div>
+     
 <?php endforeach ?>
 
 
