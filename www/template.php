f3136f64f285cbf3692c6bfdef49bfc839e4c4d7
Gary Straughan <gary@brainboxweb.co.uk>
2010-12-14 21:49:11 +0000

    more IE fixing (I hope)

--- www/template.php
+++ www/template.php
@@ -1,3 +1,4 @@
+<?php $version = 123 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
@@ -16,9 +17,9 @@
 <meta name="description" content="<?php echo $metaDescription ?>" />
 
 
-<link href="/css/s.css?10-12" type="text/css" rel="stylesheet"  />
+<link href="/css/s.css?<?php echo $version ?>" type="text/css" rel="stylesheet"  />
 <!--[if lt IE 8]>
-<link rel="stylesheet" type="text/css" media="all" href="/css/ie.css" />
+<link rel="stylesheet" type="text/css" media="all" href="/css/ie.css?<?php echo $version ?>" />
 <![endif]-->
 
 <?php
